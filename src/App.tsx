957f7260bc863c18fa163bc8997f1caea47ca2e2
Andy Montalvo <59586600+amontalvof@users.noreply.github.com>
2023-08-28 08:39:55 -0400

    feat: Date Range Picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import './App.css';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import MuiTabs from './components/MuiTabs';
-// import MuiPicker from './components/MuiPicker';
-// import MuiLoadingButton from './components/MuiLoadingButton';
-// import MuiSkeleton from './components/MuiSkeleton';
-// import MuiProgress from './components/MuiProgress';
-// import MuiDialog from './components/MuiDialog';
-// import MuiSnackbar from './components/MuiSnackbar';
-// import MuiAlert from './components/MuiAlert';
-// import MuiTable from './components/MuiTable';
-// import MuiTooltip from './components/MuiTooltip';
-// import MuiChip from './components/MuiChip';
-// import MuiList from './components/MuiList';
-// import MuiBadge from './components/MuiBadge';
-// import MuiAvatar from './components/MuiAvatar';
-// import MuiBottomNavigation from './components/MuiBottomNavigation';
-// import MuiSpeedDial from './components/MuiSpeedDial';
-// import MuiDrawer from './components/MuiDrawer';
-// import MuiBreadcrumbs from './components/MuiBreadcrumbs';
-// import MuiLink from './components/MuiLink';
-// import MuiMenu from './components/MuiMenu';
-// import MuiNavbar from './components/MuiNavbar';
-// import MuiImageList from './components/MuiImageList';
-// import MuiAccordion from './components/MuiAccordion';
-// import MuiCard from './components/MuiCard';
-// import MuiPaper from './components/MuiPaper';
-// import MuiGrid from './components/MuiGrid';
-// import MuiStack from './components/MuiStack';
-// import MuiBox from './components/MuiBox';
+import "./App.css";
+
+// import MuiTabs from "./components/MuiTabs";
+import MuiRangePicker from "./components/MuiRangePicker";
+// import MuiPicker from "./components/MuiPicker";
+// import MuiLoadingButton from "./components/MuiLoadingButton";
+// import MuiSkeleton from "./components/MuiSkeleton";
+// import MuiProgress from "./components/MuiProgress";
+// import MuiDialog from "./components/MuiDialog";
+// import MuiSnackbar from "./components/MuiSnackbar";
+// import MuiAlert from "./components/MuiAlert";
+// import MuiTable from "./components/MuiTable";
+// import MuiTooltip from "./components/MuiTooltip";
+// import MuiChip from "./components/MuiChip";
+// import MuiList from "./components/MuiList";
+// import MuiBadge from "./components/MuiBadge";
+// import MuiAvatar from "./components/MuiAvatar";
+// import MuiBottomNavigation from "./components/MuiBottomNavigation";
+// import MuiSpeedDial from "./components/MuiSpeedDial";
+// import MuiDrawer from "./components/MuiDrawer";
+// import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
+// import MuiLink from "./components/MuiLink";
+// import MuiMenu from "./components/MuiMenu";
+// import MuiNavbar from "./components/MuiNavbar";
+// import MuiImageList from "./components/MuiImageList";
+// import MuiAccordion from "./components/MuiAccordion";
+// import MuiCard from "./components/MuiCard";
+// import MuiPaper from "./components/MuiPaper";
+// import MuiGrid from "./components/MuiGrid";
+// import MuiStack from "./components/MuiStack";
+// import MuiBox from "./components/MuiBox";
 // import MuiAutocomplete from './components/MuiAutocomplete';
 // import MuiRating from './components/MuiRating';
 // import MuiSwitch from './components/MuiSwitch';
@@ -39,13 +39,13 @@ import MuiTabs from './components/MuiTabs';
 // import MuiButton from './components/MuiButton';
 // import MuiTypography from './components/MuiTypography';
 
+// import Test from "./components/Test";
+
 function App() {
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <div className="App">
-                <MuiTabs />
-            </div>
-        </LocalizationProvider>
+        <div className="App">
+            <MuiRangePicker />
+        </div>
     );
 }
 
--- src/components/MuiPicker.tsx
+++ src/components/MuiPicker.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
-import { Stack } from '@mui/material';
-import { DatePicker, TimePicker, DateTimePicker } from '@mui/x-date-pickers';
+import { useState } from "react";
+import { Stack } from "@mui/material";
+import { DatePicker, TimePicker, DateTimePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const MuiPicker = () => {
     const [selectedDate, setSelectedDate] = useState<Date | null>(null);
@@ -18,23 +20,13 @@ const MuiPicker = () => {
     );
 
     return (
-        <Stack spacing={4} sx={{ width: '250px' }}>
-            <DatePicker
-                label="Date Picker"
-                value={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-            />
-            <TimePicker
-                label="Time Picker"
-                value={selectedTime}
-                onChange={(time) => setSelectedTime(time)}
-            />
-            <DateTimePicker
-                label="Date Time Picker"
-                value={selectedDateTime}
-                onChange={(dateTime) => setSelectedDateTime(dateTime)}
-            />
-        </Stack>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Stack spacing={4} sx={{ width: "250px" }}>
+                <DatePicker label="Date Picker" value={selectedDate} onChange={(date) => setSelectedDate(date)} />
+                <TimePicker label="Time Picker" value={selectedTime} onChange={(time) => setSelectedTime(time)} />
+                <DateTimePicker label="Date Time Picker" value={selectedDateTime} onChange={(dateTime) => setSelectedDateTime(dateTime)} />
+            </Stack>
+        </LocalizationProvider>
     );
 };
 
